32f2b4a043b3dc17a3d03722b8e60bd75de7ebab
antonin <antoninartaud@hotmail.fr>
2021-04-07 15:11:19 +0200

    etape 1

---
+++ chapitre_1/elsy/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
+
+class App extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
