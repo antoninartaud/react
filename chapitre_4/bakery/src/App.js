65bdb91caa4a1235411872f8229c8948064c039b
antonin <antoninartaud@hotmail.fr>
2021-04-14 16:43:06 +0200

    Etape1 ok

---
+++ chapitre_4/bakery/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return <h1>Salut</h1>;
+  }
+}
+
+export default App;
