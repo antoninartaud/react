c2b4e9977089467f54bd66b095651fa36ea5f49a
antonin <antoninartaud@hotmail.fr>
2021-04-16 11:52:10 +0200

    Etape 4 ok

--- chapitre_4/bakery/src/components/Add.jsx
+++ chapitre_4/bakery/src/components/Add.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 
 class Add extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      productName: '',
+      price: 1,
+    };
+  }
+  updateProductName(e) {
+    console.log("i'm in updateProductName");
+    console.log('e.target.value before setState', e.target.value);
+
+    this.setState({
+      productName: e.target.value,
+    });
+  }
+
+  updatePrice(e) {
+    console.log("i'm in updatePice");
+
+    this.setState({
+      productName: e.target.value,
+    });
+  }
+
   render() {
-    return <div>add</div>;
+    console.log('this.state.productName after render', this.state.productName);
+    return (
+      <>
+        <input type='text' onChange={this.updateProductName} />
+        <input type='range' min={1} max={10} onChange={this.updatePrice} />
+        <button onClick=''>Add</button>
+        <div>add</div>
+      </>
+    );
   }
 }
 
