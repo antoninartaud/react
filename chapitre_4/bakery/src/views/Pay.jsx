03f1cfdb019a3c729254326ddc59ab8add16ccae
antonin <antoninartaud@hotmail.fr>
2021-04-30 17:29:39 +0200

    09.3 Pay ok

--- chapitre_4/bakery/src/views/Pay.jsx
+++ chapitre_4/bakery/src/views/Pay.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-// import Card from '../components/Card';
+import Card from '../components/Card';
 
 class Pay extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      basket: [],
+      total: 0,
+      totalTVA: 0,
+      totalEcoTax: 0,
+      totalTTC: 0,
+    };
+  }
+
+  handleSelect(name, price) {
+    console.log(`I'm in handleSelect! name: ${name} and price:${price}`);
+  }
+
   render() {
+    console.log(this.props.items);
     return (
       <>
         <div>Pay</div>
+        <div>
+          <p>{this.state.total}</p>
+        </div>
+
         {/* <Card itemName='croissant' /> */}
       </>
     );
