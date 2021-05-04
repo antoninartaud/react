1e589232ead73a04eb29e2c51a86e3bad8a5a03e
antonin <antoninartaud@hotmail.fr>
2021-05-04 17:33:59 +0200

    Création components et liaison ok

--- chapitre_6/catalog/src/App.js
+++ chapitre_6/catalog/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MovieDetails from './view/MovieDetails';
+import MoviesTitleList from './view/MoviesTitleList.jsx';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      titleName: '',
+    };
+    // this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  // clickHandler() {}
+
   render() {
-    return <div className={'display-1'}>Hello</div>;
+    return (
+      <div>
+        <h1>Catalogue</h1>
+        <MoviesTitleList />
+        {/* <MovieDetails /> */}
+      </div>
+    );
   }
 }
 
---
+++ chapitre_6/catalog/src/view/MovieDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Movie extends React.Component {
+  render() {
+    return <p className={'display-1'}>Boy meets girl</p>;
+  }
+}
+
+export default Movie;
