ab33594893ea6a8562add14f6b04faf48edcc707
antonin <antoninartaud@hotmail.fr>
2021-05-05 17:17:08 +0200

    Etape 3 Popular

--- chapitre_6/moovice/src/views/Popular.jsx
+++ chapitre_6/moovice/src/views/Popular.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 
 class Popular extends Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount() {
+    fetch(
+      'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e441f8a3a151d588a4932d2c5d310769'
+    )
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          movies: data,
+        });
+      });
+  }
+
   render() {
+    console.log('this.state in render :', this.state);
     return <h1>Popular</h1>;
   }
 }
